b6eb1441b6c135b246f7f12d0d20213e8c4542b9
GiorgiKanashvili <kanashviligiorgi9@gmail.com>
2024-05-25 20:43:37 +0400

    second commit

---
+++ Day 1/Homework/level1.py
@@ -0,0 +1,22 @@
+name = "giorgi"
+surname = "kanashvili"   #string = ტიპის ცვლადი. 
+age = 16       #integer = მთელი რიცხვი.
+height = 175.7     #float = ათწილადი ანუ.
+knows_english = True     #booLean ის ტიპის ცვლადი.
+is_brainless = False    #snackcase (წერის სტილი).
+
+isBrainless = False      # javascript ის წერის სტილი camelcase.
+
+
+
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_english))
+
+print("my name is" + " " + (name) + " " + (surname) + "," + " " "i'm" + " " + str(age) 
+      + " " + "years old."+ " " + "my height is" +  " " + str(height)+ " " +
+        "and i know English language," + " " + str(knows_english)+ "." + " " +
+          "soo im not brainless," + " " + str(isBrainless) + ".")
+
+
