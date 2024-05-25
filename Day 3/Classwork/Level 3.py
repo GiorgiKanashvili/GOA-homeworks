cf44fea7fbee8aacde88d5fe2ca1793b6d4eeab4
GiorgiKanashvili <kanashviligiorgi9@gmail.com>
2024-05-25 20:45:29 +0400

    third commit

---
+++ Day 3/Classwork/Level 3.py
@@ -0,0 +1,37 @@
+num1 = 5
+num2 = 4
+num3 = 5.5
+
+
+print(num1 + num2)
+print(num2 // num3)
+print(num3 ** num2)
+print(num1 * num3)
+print(num1 // num2)
+print(num3 * num1)
+print(num2 % num2)
+print(num3 * num2)
+print(num1 + num2)
+print(num2 // num3)
+print(num3 * num1)
+print(num2 % num2)
+print(num1 * num3)
+print(num1 // num2)
+print(num3 * num1)
+
+
+
+a = "Me"
+b = "You"
+print(a + b)
+
+
+name = "giorgi"
+age = 16
+height = 1,70
+Know_English = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(Know_English))
---
+++ Day 3/Homework/Level 3.py
@@ -0,0 +1,19 @@
+
+# creating memories & variabless
+
+
+
+success = "Money"
+laptop = "Studying,"
+out = "from:"
+discipline = "hard working,"
+great_body= "training,"
+goes = "comes"
+learn = "knowledge,"
+habits = "with good behavior,"
+religion = "belive in god"
+family = "and being good son <3"
+
+#now we're doing concatenate
+
+print(success, goes, out, laptop, discipline, great_body, habits, learn, religion, family) 
