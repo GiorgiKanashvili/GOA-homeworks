14915ae1b4f056de4447d84c364d57066bd3f2a3
GiorgiKanashvili <kanashviligiorgi9@gmail.com>
2024-06-25 13:28:20 +0400

    seventh commit

---
+++ Level6/Classwork/Level6.py
@@ -0,0 +1,58 @@
+#name = input("Please enter your name: ")
+
+#print("your name is " + name)
+
+#age = int(input("your age wil be doubled, enter your age: "))
+
+#print(age * 2)
+
+# age = int(input("your age will be divided in half, please enter your age: "))
+
+# print(age / 2)
+
+# age = int(input("This will be your age in 20 years, please enter your age: "))
+
+# print(age + 20)
+
+# age = int(input("This will be younger by 10, enter your age: "))
+
+# print(age - 10)
+
+# num1 = int(input("enter the number: "))
+# num2 = int(input("enter the number: "))
+
+# print(num1 + num2)
+
+
+# num1 = int(input("enter the number: "))
+# num2 = int(input("enter the number: "))
+
+# print(num1 - num2)
+
+
+# num1 = int(input("enter the number: "))
+# num2 = int(input("enter the number: "))
+
+# print(num1 * num2)
+
+
+# num1 = int(input("enter the number: "))
+# num2 = int(input("enter the number: "))
+
+# print(num1 / num2)
+
+
+# name = "Giorgi"
+# lastname = "kana"
+# age = 44
+
+# print(f"my name is {name}, my lastname is {lastname}, my age is {age}.")
+
+
+name = input("Enter your name: ")
+lastname = input("Enter your last name: ")
+age = int(input("Enter your age: "))
+
+#print(f"your name is {name}, your lastname is {lastname}, your age is {age}.")
+
+print("your name is", name, "your lastame is", lastname, "your age is", age)
---
+++ Level6/Homework/Level6.py
@@ -0,0 +1,28 @@
+name = input("Enter your name : ")
+lastname = input("Enter your lastname: ")
+age = input("Enter your age: ")
+gmail = input("Enter your gmail: ")
+
+
+print(f"Your name is {name}. Your lastname is {lastname}. Your age is {age}. Your gmail is {gmail}.")
+
+
+num1 = int(input("Enter the number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+print(num1 + num2 - num3)
+print(num2 * num3 + num1)
+print(num3 - num1 * num2)
+print(num2 / num3 + num2)
+
+
+
+price = int(input("Enter the price: "))
+quantity = int(input("Enter the quantity: "))
+
+print(price * quantity)
+
+
+
+
